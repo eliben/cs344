4cd545bba4f4f12b49ef055761ae1c787ea343dd
Eli Bendersky <eliben@chromium.org>
2013-12-06 06:45:42 -0800

    first signs of life - correct index computations!

--- HW1/student_func.cu
+++ HW1/student_func.cu
@@ -50,15 +50,32 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+
+  // Compute the thread's row and column
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (col >= numCols || row >= numRows) {
+    return;
+  }
+
+  // Offset into the 1D images
+  int offset = row * numCols + col;
+  greyImage[offset] = rgbaImage[offset].x;
 }
 
-void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
-                            unsigned char* const d_greyImage, size_t numRows, size_t numCols)
+void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage,
+                            uchar4 * const d_rgbaImage,
+                            unsigned char* const d_greyImage,
+                            size_t numRows, size_t numCols)
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
-  const dim3 gridSize( 1, 1, 1);  //TODO
+
+  // TODO: gridSize should probably have its size computed according to
+  // image size?
+  const dim3 blockSize(16, 16, 1);  //TODO
+  const dim3 gridSize( 80, 80, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
